8aa716cfe7d7ff06880a3d22158265ed5747fefb
武万浩 <49355179+wuwanhao@users.noreply.github.com>
2020-02-25 17:54:48 +0800

    first commit

---
+++ src/test/java/com/excelanalysis/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelanalysis;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
